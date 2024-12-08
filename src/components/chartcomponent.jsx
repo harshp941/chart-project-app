b7b33963f9bcc5b4597e13ce91a3c0ec01211908
Harsh Patel <harshp5@usf.edu>
2024-12-08 12:51:22 -0500

    fix

--- src/components/chartcomponent.jsx
+++ src/components/chartcomponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Chart } from 'chart.js';
+import { Chart } from 'chart.js/auto';
 
 const ChartComponent = ({ type, data, options }) => {
   const chartRef = useRef(null);
